50b20ba9593264f5ec4d0db0efb98197c91a6a30
MootSeeker <mootseeker98@gmail.com>
2018-07-01 00:31:53 +0200

    Add files via upload

---
+++ SD_Card.ino
@@ -0,0 +1,42 @@
+// SD Karte ansteuern
+
+#include <SPI.h>
+#include <SD.h>
+
+
+File sdFile; 
+
+const uint8_t csPin = 10;
+ 
+void setup() 
+{
+  Serial.println(115200); 
+
+  pinMode(csPin, OUTPUT); 
+
+  if(SD.begin())
+  {
+    Serial.print("SD is ready"); 
+  }
+  else
+  {
+    Serial.println("SD card not found!"); 
+  }
+
+  sdFile = SD.open("test.csv", FILE_WRITE); 
+
+  if(sdFile)
+  {
+    Serial.println("Write to SD card"); 
+
+    sdFile.println("Dies ist ein Test String. "); 
+    sdFile.close(); 
+
+    Serial.println("Done!"); 
+  }
+}
+
+void loop() 
+{
+  // Nothing
+}
